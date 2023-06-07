f7525640c504e584de653c9165bd4f103182e6aa
SayhanA <sayhanahmed05@gmail.com>
2023-06-08 05:49:13 +0600

    Server side create and all midleware added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.u2hpa9s.mongodb.net/?retryWrites=true&w=majority`;
+
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the server")
